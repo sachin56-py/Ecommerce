3404d99dc370aee42ddfc8511d742e54e37fd8a2
sachinmnm10 <sachinmnm10@gmail.com>
2023-01-03 17:01:33 +0545

    created user models

--- Ecommerce/products/models.py
+++ Ecommerce/products/models.py
@@ -4,8 +4,8 @@ import uuid
 
 class BaseModel(models.Model):
     uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    created_at = models.DateTimeField(auto_now_add=True)  # will add date and time when it is created
-    updated_at = models.DateTimeField(auto_now=True) # will add date and time when it is updated
+    created_at = models.DateTimeField(auto_now=True)  # will add date and time when it is created
+    updated_at = models.DateTimeField(auto_now_add=True) # will add date and time when it is updated
 
     class Meta:   # we don not want django to make a model from here but only to act as a class
         abstract = True

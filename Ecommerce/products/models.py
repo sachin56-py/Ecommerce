4cf903f52631c1c57d2d8aefdaea57e615fa8b06
sachinmnm10 <sachinmnm10@gmail.com>
2023-01-03 16:47:34 +0545

    pillow is installed to handle image files

--- Ecommerce/products/models.py
+++ Ecommerce/products/models.py
@@ -1,23 +1,33 @@
 from django.db import models
-from backup.models import Basemodel
-
+import uuid
 # Create your models here.
-class Category(Basemodel):
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)  # will add date and time when it is created
+    updated_at = models.DateTimeField(auto_now=True) # will add date and time when it is updated
+
+    class Meta:   # we don not want django to make a model from here but only to act as a class
+        abstract = True
+
+
+
+class Category(BaseModel):
     category_name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, null=True, blank=true)
-    category_image = models.ImageField(upload="categories")
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    category_image = models.ImageField(upload_to="categories")
 
 
 
-class Product(Basemodel):
+class Product(BaseModel):
     product_name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product")
-    price = models.IntegerField(max_length=100)
+    price = models.IntegerField()
     product_description = models.TextField()
     slug = models.SlugField(unique=True, null=True, blank=True)
 
 
 
-class ProductImage(Basemodel):
+class ProductImage(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image")
-    image = models.ImageField(upload="product")
+    image = models.ImageField(upload_to="product")
